c100173c49c91a95782b05701c987dbe0f2b9cc2
Meer Aymaan Ali <150371218+meer3305@users.noreply.github.com>
2026-05-15 20:29:20 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l=0;
+        int r=1;
+        int maxp=0;
+        int profit=0;
+        while (r < prices.length){
+            if (prices[l]<prices[r]){
+                profit=prices[r]-prices[l];
+                maxp=Math.max(maxp,profit);
+            }
+            else{
+                l=r;
+            }
+            r+=1;
+        }
+        return maxp;
+    }
+}
